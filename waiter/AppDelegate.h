c9b68c02b12999d3ae0e3ae2fdb7ec3a596f3cf7
LC0524 <793966766@qq.com>
2016-01-06 11:48:30 +0800

    申请仓库
    创建项目，申请仓库

---
+++ waiter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  waiter
+//
+//  Created by 刘超 on 16/1/6.
+//  Copyright © 2016年 Lc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ waiter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  waiter
+//
+//  Created by 刘超 on 16/1/6.
+//  Copyright © 2016年 Lc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
